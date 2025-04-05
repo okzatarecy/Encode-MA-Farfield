3972588df7cb52fa8a83a0a7f7414fbfb89ef7c9
Okzata Recy <okzatarecy@gmail.com>
2025-04-05 18:26:11 -0230

    edit graph

--- QGCN_FAS.py
+++ QGCN_FAS.py
@@ -11,6 +11,7 @@ from qiskit.circuit import QuantumRegister, ClassicalRegister, QuantumCircuit, P
 from qiskit import transpile
 from qiskit.primitives import StatevectorSampler, StatevectorEstimator
 from qiskit.result import marginal_counts
+from qiskit.circuit import Gate
 from math import pi
 import scipy
 from scipy.special import jv
@@ -83,12 +84,18 @@ import matplotlib.pyplot as plt
 # 1. Data Preparation
 # ==============================
 # Channel matrix H (3 ports x 2 antennas)
+# H = np.array([
+#     [0.8+0.1j, 0.2-0.3j],  # Port 1
+#     [0.3+0.4j, 0.7-0.2j],  # Port 2
+#     [0.1+0.5j, 0.4-0.1j]   # Port 3
+# ])
 H = np.array([
-    [0.8+0.1j, 0.2-0.3j],  # Port 1
-    [0.3+0.4j, 0.7-0.2j],  # Port 2
-    [0.1+0.5j, 0.4-0.1j]   # Port 3
+    [0.8+0.1j],  # Port 1
+    [0.3+0.4j],  # Port 2
+    [0.1+0.5j]   # Port 3
 ])
-
+H_real = np.real(H).flatten()
+H_imag = np.imag(H).flatten()
 # Adjacency matrix (nodes 0-1: antennas, 2-4: ports)
 A = np.array([
     [0, 0, 1, 1, 1],  # Antenna 0
@@ -98,103 +105,151 @@ A = np.array([
     [1, 1, 0, 0, 0]   # Port 3
 ])
 
-# ==============================
-# 2. Quantum Circuit Construction
-# ==============================
-def build_qgcn(H, A, num_layers=1):
-    num_nodes = len(A)
-    q = QuantumRegister(num_nodes, 'q')
-    c = ClassicalRegister(3, 'c')
-    qc = QuantumCircuit(q,c)
+
+# %% quantum circuit
+
+def Q_encode(H, H_real, H_imag, w_1, w_2, w_3):
     
-    # Normalize amplitudes
-    h_abs = np.abs(H)
-    h_max = np.max(h_abs)
+    q1 = QuantumRegister(H.size, 'q1')
+    qc1 = QuantumCircuit(q1)
     
-    # Encode channel information
-    # for port in range(3):
-    #     for ant in range(2):
-    #         # Amplitude encoding (safe normalization)
-    #         angle_amp = (h_abs[port, ant] / h_max) * np.pi
-    #         qc.ry(angle_amp, ant)
+    q2 = QuantumRegister(4, 'q2')
+    c2 = ClassicalRegister(1, 'c2')
+    qc2 = QuantumCircuit(q2,c2)
+    
+    final_qc = QuantumCircuit(q1, q2, c2)  # Deklarasi semua register sekaligus
+    final_qc = final_qc.compose(qc1)       # Tambahkan qc1
+    final_qc = final_qc.compose(qc2)       # Tambahkan qc2
+    
+    # quantum state preparation
+    for i_re in range(H.size): 
+        final_qc.ry(H_real[i_re], q1[i_re])
+        
+    for i_im in range(H.size):
+        final_qc.rz(H_imag[i_im], q1[i_im])
+        
+    final_qc.barrier()
+    
+    def create_u_gate(label,w_i):
+        u = QuantumCircuit(2, name=f'U{label}')
+        u.cx(1,0)
+        u.rz(np.pi/4, 0)
+        u.ry(w_i, 1)
+        u.cx(0, 1)
+        u.rz(np.pi/4, 1)
+        u.cx(1,0)
+        return u.to_gate()
+    
+    u1 = create_u_gate(1, w_1).control(1)  #control(1) menambah 1 control di Ugate,
+    final_qc.append(u1, [q1[0], q2[0], q2[1]])
+    final_qc.swap(q2[0], q2[1])
+    
+    u2 = create_u_gate(2, w_2).control(1)
+    final_qc.append(u2, [q1[1], q2[1], q2[2]])
+    final_qc.swap(q2[1], q2[2])
+    
+    u3 = create_u_gate(3, w_3).control(1)
+    final_qc.append(u3, [q1[2], q2[2], q2[3]])
+    
+    final_qc.barrier()
+    
+    final_qc.cx(q2[2],q2[3])
+    final_qc.measure(q2[3], c2[0])
+    
+    return final_qc
+w_1 = 0
+w_2 = 0
+w_3 = 0
+qc2 = Q_encode(H, H_real, H_imag, w_1, w_2, w_3)
+
+
+# def build_qgcn(H, A, num_layers=1):
+#     num_nodes = len(A)
+#     q = QuantumRegister(num_nodes, 'q')
+#     c = ClassicalRegister(3, 'c')
+#     qc = QuantumCircuit(q,c)
+    
+#     # Normalize amplitudes
+#     h_abs = np.abs(H)
+#     h_max = np.max(h_abs)
+    
+#     # Encode channel information
+#     # for port in range(3):
+#     #     for ant in range(2):
+#     #         # Amplitude encoding (safe normalization)
+#     #         angle_amp = (h_abs[port, ant] / h_max) * np.pi
+#     #         qc.ry(angle_amp, ant)
             
 
-    #         # Phase encoding
-    #         angle_phase = np.angle(H[port, ant])
-    #         qc.rz(angle_phase, ant)
-    #         qc.barrier()
-    # qc.barrier()
+#     #         # Phase encoding
+#     #         angle_phase = np.angle(H[port, ant])
+#     #         qc.rz(angle_phase, ant)
+#     #         qc.barrier()
+#     # qc.barrier()
     
     
-    for port in range(3):
-       # Gabungkan informasi kedua antenna
-       combined_angle = np.arctan(np.sum(h_abs[port,:])/h_max * np.pi/2)
-       qc.ry(combined_angle, port+2)  # Qubit 2-4 adalah port
+#     for port in range(3):
+#        # Gabungkan informasi kedua antenna
+#        combined_angle = np.arctan(np.sum(h_abs[port,:])/h_max * np.pi/2)
+#        qc.ry(combined_angle, port+2)  # Qubit 2-4 adalah port
        
-       # Encoding fase
-       mean_phase = np.mean(np.angle(H[port,:]))
-       qc.rz(mean_phase, port+2)
-       qc.barrier()
+#        # Encoding fase
+#        mean_phase = np.mean(np.angle(H[port,:]))
+#        qc.rz(mean_phase, port+2)
+#        qc.barrier()
     
-    # Parameterized layers
-    theta = ParameterVector('θ', length=num_layers*(2*num_nodes + 1))
+#     # Parameterized layers
+#     theta = ParameterVector('θ', length=num_layers*(2*num_nodes + 1))
     
-    for layer in range(num_layers):
-        # Node feature transformation
-        for i in range(num_nodes):
-            qc.ry(theta[layer*num_nodes + i], i)
-            qc.rz(theta[layer*num_nodes + num_nodes + i], i)
-        qc.barrier()
-        # Graph convolution
-        for i in range(num_nodes):
-            for j in range(i+1, num_nodes):
-                if A[i,j] == 1:
-                    qc.cz(i, j)
-                    qc.cry(theta[-1], i, j)  # Shared parameter
-                    qc.barrier()
+#     for layer in range(num_layers):
+#         # Node feature transformation
+#         for i in range(num_nodes):
+#             qc.ry(theta[layer*num_nodes + i], i)
+#             qc.rz(theta[layer*num_nodes + num_nodes + i], i)
+#         qc.barrier()
+#         # Graph convolution
+#         for i in range(num_nodes):
+#             for j in range(i+1, num_nodes):
+#                 if A[i,j] == 1:
+#                     qc.cz(i, j)
+#                     qc.cry(theta[-1], i, j)  # Shared parameter
+#                     qc.barrier()
                     
-    qc.measure(q[2], c[0])
-    qc.measure(q[3], c[1])
-    qc.measure(q[4], c[2])
-    return qc,theta
+#     qc.measure(q[2], c[0])
+#     qc.measure(q[3], c[1])
+#     qc.measure(q[4], c[2])
+#     return qc,theta
 
-circut,theta = build_qgcn(H, A, num_layers=1)
+# circut,theta = build_qgcn(H, A, num_layers=1)
 
 # %%
-
-# ==============================
-# 3. Training with Parameter-Shift
-# ==============================
 def ave_meas(count):
      total = count.get('0', 0) + count.get('1', 0)
      return count.get('1', 0) / total if total > 0 else 0
  
-def Q_decode(H, A, shots):
+def Q_decode(H, H_real, H_imag, w_1, w_2, w_3, shots):
     
-    qc, theta = build_qgcn(H, A, num_layers=1)
-    qc.draw()
+    qc = Q_encode(H, H_real, H_imag, w_1, w_2, w_3)
     sampler = StatevectorSampler() 
     
     job = sampler.run( [(qc)], shots=shots)
     result = job.result()
-    counts_sam = result[0].data.c.get_counts()
+    counts_sam = result[0].data.c2.get_counts()
     
-    simp_counts_03 = marginal_counts(counts_sam, indices=[2])
-    simp_counts_04 = marginal_counts(counts_sam, indices=[3])
-    simp_counts_05 = marginal_counts(counts_sam, indices=[4])
-    
-    out3 = ave_meas(simp_counts_03)
-    out4 = ave_meas(simp_counts_04)
-    out5 = ave_meas(simp_counts_05)
-    
-    out = [out3, out4, out5]
-    return out, out3,out4, out5
+    simp_counts_01 = marginal_counts(counts_sam, indices=[0])
 
-out, out3,out4, out5 = Q_decode(H, A, shots =1024)
+    
+    out1 = ave_meas(simp_counts_01)
+    
+    out = [out1]
+    return qc, out, out1
+
+w_1 = np.pi
+w_2 = np.pi
+w_3 = np.pi
+qc, out, out1 = Q_decode(H, H_real, H_imag, w_1, w_2, w_3, shots=1024)
 
 print("measurement_average_01 =",out[0])
-print("measurement_average_02 =",out[1])
-print("measurement_average_03 =",out[2])
 
 
 # %%
